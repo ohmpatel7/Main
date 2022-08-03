f5dfe4a5605365fb5fc251e383336988ee149f47
ohmpatel7 <98992395+ohmpatel7@users.noreply.github.com>
2022-08-03 16:30:06 +1200

    Update importedfilequestions.py

--- importedfilequestions.py
+++ importedfilequestions.py
@@ -2,8 +2,8 @@ def load_questions_from_file(file_name):
     """
     Load questions from a file.
     """
-    with open(file_name, 'r', ) as FILE:
-        for line in FILE:
+    with open(file_name, 'r',) as file:
+        for line in file:
             entry = line.split(' ! ')
             if entry[-1].endswith('\n'):
                 entry[-1] = entry[-1][:-1]
@@ -11,9 +11,8 @@ def load_questions_from_file(file_name):
                 entry[-1] = entry[-1][:-1]
             options = entry[1].split(", ")
             answers = entry[2].split(", ")
-            elements = []
             try:
                 elements.append([entry[0], options, answers])
             except (NameError, UnboundLocalError):
-                elements:[[entry[0], options, answers]]
+                elements: list = [[entry[0], options, answers]]
     return elements
